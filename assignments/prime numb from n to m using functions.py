ce0c08e00fb77afbd7c56ebe53d6589c9adcf6a5
SANIYA-05 <68528927+SANIYA-05@users.noreply.github.com>
2020-08-02 18:04:55 +0530

    commit

---
+++ assignments/prime numb from n to m using functions.py
@@ -0,0 +1,14 @@
+def prime():
+    n = int(input())
+    m = int(input())
+    while n <= m:
+        count = 0
+        i=1
+        while i <= n:
+            if n % i == 0:
+                count += 1
+            i=i+1
+        if count==2:
+            print(n)
+        n+=1
+prime()
