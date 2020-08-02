6cb81c1c15ab16391574c9c45eae1f54b94a0c35
SANIYA-05 <68528927+SANIYA-05@users.noreply.github.com>
2020-08-02 18:01:42 +0530

    commit

---
+++ assignments/prime numbers using function without for loop.py
@@ -0,0 +1,13 @@
+def prim():
+    n=int(input())
+    i=1
+    count=0
+    while(i<=n):
+        if(n%i==0):
+            count+=1
+        i+=1
+    if(count==2):
+        print("number is prime")
+    else:
+        print("number not prime")
+prim()
